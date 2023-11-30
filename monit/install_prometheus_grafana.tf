a209fb8c7db9115b06f953af59d34209f9eceda3
rfdias <reinaldo@rfdias-2.local>
2023-11-30 11:14:17 -0300

    testing new install method for prometheus

--- monit/install_prometheus_grafana.tf
+++ monit/install_prometheus_grafana.tf
@@ -1,8 +1,19 @@
 //criar namespace da monitoração
 
-//instalar o helm do grafana/prometheus
-resource "null_resource" "jenkins" {
-  depends_on = [module.ebs, null_resource.namespace]
+//Local Backend
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+
+resource "null_resource" "monit" {
+
   provisioner "local-exec" {
     command = <<EOT
       helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
@@ -13,3 +24,4 @@ resource "null_resource" "jenkins" {
       kubectl get secret --namespace monit monit-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
     EOT
   }
+}
