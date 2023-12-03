8c9b579e391be80a9780717dd47bcc9782f726de
rfdias <reinaldo@rfdias-2.local>
2023-12-03 12:26:04 -0300

    prometheus install script update

--- monit/install_prometheus_grafana.tf
+++ monit/install_prometheus_grafana.tf
@@ -16,11 +16,13 @@ resource "null_resource" "monit" {
 
   provisioner "local-exec" {
     command = <<EOT
+      kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
       helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
       helm repo update
       helm install monit prometheus-community/kube-prometheus-stack
       kubectl create ns monit
       cd ../monit   ; helm upgrade --install monit prometheus-community/kube-prometheus-stack --namespace monit -f values.yaml
+      kubectl top pod --namespace monit 
       kubectl get secret --namespace monit monit-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
     EOT
   }
