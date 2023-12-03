a2e71020966273221d7761bd6e03641e40c5f023
rfdias <reinaldo@rfdias-2.local>
2023-12-03 15:14:44 -0300

    provider update

--- infra/provider.tf
+++ infra/provider.tf
@@ -36,27 +36,27 @@ terraform {
 
 
 // s3 bucket for tf remote state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "my-devops-project-01-tf-state-2024"
-  force_destroy = true
-  tags = {
-    Name = "TF state bucket"
-  }
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket        = "my-devops-project-01-tf-state-2024"
+#   force_destroy = true
+#   tags = {
+#     Name = "TF state bucket"
+#   }
 
-  lifecycle {
-    prevent_destroy = true
-  }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
 
-      }
-    }
-  }
+#       }
+#     }
+#   }
 
-}
+# }
 //DynameDB table for terraform state
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-locking"
